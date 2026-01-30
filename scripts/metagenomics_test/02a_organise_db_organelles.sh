576ddbf4e0a6c8dc96b6debb8f3970663667a707
mmontonerin <merce.montoliu@gmail.com>
2026-01-30 14:26:22 +0000

    setting up for diamond-blastx organelles

---
+++ scripts/metagenomics_test/02a_organise_db_organelles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#BSUB -o ./log/ftpdownload-%J-output.log
+#BSUB -e ./log/ftpdownload-%J-error.log 
+#BSUB -J ncbi_ftpdownload
+#BSUB -q normal
+#BSUB -G team301
+#BSUB -n 1
+#BSUB -W 04:00
+#BSUB -M 10000
+#BSUB -R "select[mem>10000] rusage[mem=10000]"
+
+rsync -av --timeout=600 rsync://ftp.ncbi.nlm.nih.gov/refseq/release/plastid/ refseq_plastid/
+rsync -av --timeout=600 rsync://ftp.ncbi.nlm.nih.gov/refseq/release/mitochondrion/ refseq_mitochondrion/
+
+mkdir db
+mv refseq* db/
+
+cp db/refseq*/*protein.faa.gz db/ 
+gunzip db/*protein.faa.gz
+
+cat db/*faa > organelle_proteins.faa
